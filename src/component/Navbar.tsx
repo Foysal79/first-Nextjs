e243329423316196ea0d7cd5c95362ec9786c115
Foysal79 <onthor312001@gmail.com>
2026-03-03 07:58:54 +0600

    add

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,13 +1,20 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
+  const pathname = usePathname();
+  console.log(pathname);
   return (
     <nav className="bg-blue-600 p-4 text-white flex justify-between">
       <div className="text-xl font-bold">
         <Link href="/">Nextjs</Link>
       </div>
       <div className="flex space-x-6">
-        <Link href="/" className="bg-red-900 text-white">
+        <Link
+          href="/"
+          className={`${pathname === "/" ? "bg-slate-600 text-white" : "bg-red-900 text-white"}`}
+        >
           Home
         </Link>
         <Link href="/posts" className="bg-red-900 text-white">
